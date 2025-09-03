2ebe25bc1e75303b91b5827f7408bcf44f3eef88
keshavsharma74 <keshav165p@gmail.com>
2025-09-03 15:09:21 +0530

    Type of variables in typescript

---
+++ variable.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello";
+console.log(greetings);
+greetings.toLowerCase();
---
+++ variable.ts
@@ -0,0 +1,20 @@
+const greetings : string="Hello";
+console.log(greetings);
+
+let userId:number = 33553.4;
+
+let isLoggedin:boolean=true;
+
+let hero:string;
+
+function getHero(){
+    return "Hello World"
+}
+
+hero=getHero();
+
+console.log(hero);
+
+
+greetings.toLowerCase();
+export {}
