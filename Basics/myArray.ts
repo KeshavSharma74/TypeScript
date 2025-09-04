3659c3c3a6207044ef3a032b3927b4cafbd086ba
keshavsharma74 <keshav165p@gmail.com>
2025-09-04 14:14:37 +0530

    Arrays in typescript

---
+++ Basics/myArray.ts
@@ -0,0 +1,15 @@
+const arr:string[] = []
+
+arr.push("keshav");
+arr.push("sharma");
+
+const arr1:Array<string>=[];
+arr.push("1234")
+
+type user={
+    name:string,
+    email:string
+}
+
+const arr2:user[]=[];
+arr2.push({name:"asdfg",email:"sdfghgf"})
